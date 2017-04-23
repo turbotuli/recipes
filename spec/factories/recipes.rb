0bf03118f665edc2a83f73d53e8cbe0ede154320
Tuli Jakobson <turbotuli@gmail.com>
2017-04-23 00:14:43 +0000

    Add recipe model

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :recipe do
+    name "MyString"
+    description "MyText"
+     instructions "MyText"
+    servings "MyString"
+    integer "MyString"
+    cooking_time 1
+  end
+end
